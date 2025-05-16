243291d9eb7f91b7cbdc24ae7c7bfff2729a4190
fara-t03fp24 <t03fp24@abdn.ac.uk>
2025-05-17 04:57:47 +0500

    Update run configuration to specify host and port for app execution

--- run.py
+++ run.py
@@ -4,7 +4,5 @@ import os
 app = create_app('development')
 
 if __name__ == '__main__':
-    # Get port from environment variable or default to 4555
-    port = int(os.environ.get('PORT', 4555))
-    # Run the app on port 4555 which is standard for Codio
+    port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port, debug=True)
